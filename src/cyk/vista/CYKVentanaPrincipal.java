fe2c8dc6fef59db61395fff7b7e61fea7f22d439
Eileen Guerrero <eileenguerrerogomez@gmail.com>
2018-10-30 13:55:01 -0500

    Ya todo casi listo. Solo falta mostrar la respuesta.

--- src/cyk/vista/CYKVentanaPrincipal.java
+++ src/cyk/vista/CYKVentanaPrincipal.java
@@ -2,7 +2,6 @@ package cyk.vista;
 
 import java.awt.BorderLayout;
 import java.awt.GridLayout;
-import java.awt.Label;
 import java.awt.Panel;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -14,10 +13,17 @@ import javax.swing.JLabel;
 import javax.swing.JTextArea;
 import javax.swing.JTextField;
 
+import cyk.entidades.Variable;
+import cyk.modelo.AlgoritmoCYK;
+
 
 public class CYKVentanaPrincipal extends JFrame{
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
 	private JTextArea cantidadProducciones;
-	private JButton verRespuesta;
+	private JButton verRespuestaBoton;
 	private JLabel verRespuestaLabel;
 	
 	
@@ -28,8 +34,17 @@ public class CYKVentanaPrincipal extends JFrame{
 	private final static String CANTIDAD_PRODUCCIONES="Aceptar";
 	
 	
+	/**
+	 * panel dinamico en el que se va a escribir las producciones
+	 */
 	private Panel producciones;
+	
+	private ArrayList<ArrayList<JTextField>> produccionesCadenas;
 
+	/**
+	 * enlace con el modelo
+	 */
+	private AlgoritmoCYK mundo;
 	
 	public CYKVentanaPrincipal () {
 		setSize(600, 400);
@@ -37,6 +52,7 @@ public class CYKVentanaPrincipal extends JFrame{
 		setLayout(new BorderLayout());
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		
+		mundo = new AlgoritmoCYK();
 		
 		
 		//declaracion del panel donde se van a introducir la producciones
@@ -69,28 +85,57 @@ public class CYKVentanaPrincipal extends JFrame{
 		
 		
 		Panel panelAuxInferior = new Panel();
-		panelAuxInferior.setLayout(new GridLayout(1, 2));
-		verRespuesta = new JButton(VER_RESPUESTA);
-		verRespuesta.setActionCommand(VER_RESPUESTA);
+		panelAuxInferior.setLayout(new GridLayout(1, 3));
+		JTextField cadenaAProbar = new JTextField();
+		panelAuxInferior.add(cadenaAProbar);
+		
+		verRespuestaBoton = new JButton(VER_RESPUESTA);
+		verRespuestaBoton.setActionCommand(VER_RESPUESTA);
+		verRespuestaBoton.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				Variable [] variablesEntradas = new Variable[Integer.parseInt(cantidadProducciones.getText())];
+				for (int i = 0; i < produccionesCadenas.size(); i++) {
+					Variable temporal = new Variable(produccionesCadenas.get(i).get(0).getText(), produccionesCadenas.get(i).get(1).getText().split(","));
+					variablesEntradas[i]= temporal;
+				}
+				//TODO
+				String [][] respuestaAMostrar = mundo.generaMatriz(variablesEntradas, cadenaAProbar.getText());
+				
+			}
+		});
+		
+		
 		verRespuestaLabel = new JLabel("Aun no se ha dado una respuesta");
 		
-		panelAuxInferior.add(verRespuesta);
+		panelAuxInferior.add(verRespuestaBoton);
 		panelAuxInferior.add(verRespuestaLabel);
 		
 		
 		add(panelAuxInferior, BorderLayout.SOUTH);
 		
 	}
+
+	
 	
 	public void modificarPanelProducciones (int caantidadProducciones) {
 		producciones.setLayout(new GridLayout(caantidadProducciones+1, 2) );
 		producciones.add(new JLabel("Produccion "));
 		producciones.add(new JLabel("Variables que produce"));
+		produccionesCadenas = new ArrayList<ArrayList<JTextField>>();
 		for (int i = 0; i < caantidadProducciones; i++) {
-			producciones.add(new JTextField());
-			producciones.add(new JTextField());
+			JTextField a = new JTextField();
+			JTextField b = new JTextField();
+			ArrayList<JTextField> temp = new ArrayList<JTextField>();
+			temp.add(a);
+			temp.add(b);
+			produccionesCadenas.add(temp);
+			producciones.add(produccionesCadenas.get(i).get(0));
+			producciones.add(produccionesCadenas.get(i).get(1));
+			
 		}
-		//this.remove(producciones);
+		
 		this.pack();
 	}
 	
