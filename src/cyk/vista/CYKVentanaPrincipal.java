77cc80c90a8060b602bfd9c5b9d431eaf01b633b
Eileen Guerrero <eileenguerrerogomez@gmail.com>
2018-10-30 11:35:21 -0500

    Interfaz dinamica

--- src/cyk/vista/CYKVentanaPrincipal.java
+++ src/cyk/vista/CYKVentanaPrincipal.java
@@ -4,19 +4,31 @@ import java.awt.BorderLayout;
 import java.awt.GridLayout;
 import java.awt.Label;
 import java.awt.Panel;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.ArrayList;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JTextArea;
+import javax.swing.JTextField;
 
 
 public class CYKVentanaPrincipal extends JFrame{
 	private JTextArea cantidadProducciones;
 	private JButton verRespuesta;
 	private JLabel verRespuestaLabel;
+	
+	
+	/**
+	 * Constantes de los botones
+	 */
 	private final static String VER_RESPUESTA="Ver Respuesta";
 	private final static String CANTIDAD_PRODUCCIONES="Aceptar";
+	
+	
+	private Panel producciones;
 
 	
 	public CYKVentanaPrincipal () {
@@ -26,6 +38,10 @@ public class CYKVentanaPrincipal extends JFrame{
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		
 		
+		
+		//declaracion del panel donde se van a introducir la producciones
+		producciones = new Panel();
+		
 		//panel superior que muestra un label que indica escribir la cantidad de producciones
 		Panel panelAux= new Panel();
 		panelAux.setLayout(new GridLayout(1, 3));
@@ -33,13 +49,21 @@ public class CYKVentanaPrincipal extends JFrame{
 		cantidadProducciones = new JTextArea();
 		panelAux.add(cantidadProducciones);
 		
-		JButton cantidadProducciones = new JButton(CANTIDAD_PRODUCCIONES);
-		cantidadProducciones.setActionCommand(CANTIDAD_PRODUCCIONES);
-		panelAux.add(cantidadProducciones);
+		JButton cantidadProduccionesBoton = new JButton(CANTIDAD_PRODUCCIONES);
+		cantidadProduccionesBoton.setActionCommand(CANTIDAD_PRODUCCIONES);
+		cantidadProduccionesBoton.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				modificarPanelProducciones(Integer.parseInt(cantidadProducciones.getText()));
+				add(producciones, BorderLayout.CENTER);
+				
+				
+			}
+		});
+		panelAux.add(cantidadProduccionesBoton);
 		
 		add(panelAux, BorderLayout.NORTH);
-		
-		
 		
 		
 		
@@ -57,6 +81,17 @@ public class CYKVentanaPrincipal extends JFrame{
 		
 	}
 	
+	public void modificarPanelProducciones (int caantidadProducciones) {
+		producciones.setLayout(new GridLayout(caantidadProducciones+1, 2) );
+		producciones.add(new JLabel("Produccion "));
+		producciones.add(new JLabel("Variables que produce"));
+		for (int i = 0; i < caantidadProducciones; i++) {
+			producciones.add(new JTextField());
+			producciones.add(new JTextField());
+		}
+		//this.remove(producciones);
+		this.pack();
+	}
 	
 	
 
