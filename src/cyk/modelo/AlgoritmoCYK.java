23e54505c36cb241f77c624e48fc197402f3e30a
diegolamus <diegolamus@pg2gforge.icesi.edu.co>
2018-10-30 09:58:14 -0500

    Firma método generaLaCadena

--- src/cyk/modelo/AlgoritmoCYK.java
+++ src/cyk/modelo/AlgoritmoCYK.java
@@ -1,5 +1,11 @@
 package cyk.modelo;
 
+import cyk.entidades.Produccion;
+
 public class AlgoritmoCYK {
 
+	public static boolean generaLaCadena(Produccion[] producciones, String cadena) {
+		
+		return false;
+	}
 }
